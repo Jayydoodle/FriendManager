6bc6cb6e9a499be1e5a1e11fe61f5c92bfc1d191
Jason Allen <jayydoodle@gmail.com>
2023-09-15 01:37:12 -0400

    Incrementing version number to v1.1

--- FriendManager/Program.cs
+++ FriendManager/Program.cs
@@ -7,7 +7,7 @@ using System.Configuration;
 class Program
 {
     private const string ApplicationName = "Friend Manager";
-    private const string VersionNumber = "1.0";
+    private const string VersionNumber = "1.1";
 
     static HttpClient WebClient { get; set; }
 
