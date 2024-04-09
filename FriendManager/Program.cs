99991d1a84e80643b1117440768278379d98b8fd
Jason Allen <jayydoodle@gmail.com>
2024-04-08 17:57:28 -0700

    Updating version number to v1.8

--- FriendManager/Program.cs
+++ FriendManager/Program.cs
@@ -7,7 +7,7 @@ using System.Configuration;
 class Program
 {
     private const string ApplicationName = "Friend Manager";
-    private const string VersionNumber = "1.7";
+    private const string VersionNumber = "1.8";
 
     static HttpClient WebClient { get; set; }
 
